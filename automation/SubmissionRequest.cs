b5f32572a39e1f5b26149e95455710a6a1532a3d
Andrew Gentry <andrew.gentry@colorado.edu>
2020-09-07 16:39:34 -0600

    Update wikipedia client, add github client

--- automation/SubmissionRequest.cs
+++ automation/SubmissionRequest.cs
@@ -13,40 +13,23 @@ namespace wildwikis.automation
     public class SubmissionRequest
     {
         private WikipediaClient _wikipediaClient;
+        private GithubClient _githubClient;
 
         [FunctionName("SubmissionRequest")]
         public async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation($"C# HTTP trigger function processed a Submission Request.");
             _wikipediaClient = new WikipediaClient(log);
+            _githubClient = new GithubClient(log);
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            SubmissionRequestDto submissionRequest = JsonConvert.DeserializeObject<SubmissionRequestDto>(requestBody);
 
-            var link = data?.link;
-            var comments = data?.comments;
-            log.LogInformation($"The link: {link}");
+            WikipediaArticle wikipediaArticle = await _wikipediaClient.FetchArticle(submissionRequest);
+            string pullRequest = await _githubClient.OpenNewPullRequest();
 
-            // Extract the title to use from the link
-            var uri = new Uri(link.ToString());
-            var segments = uri.Segments;
-            log.LogInformation($"The segments: {segments}");
-
-            var title = segments[2];
-            var wikipediaDescription = await _wikipediaClient.FetchArticleText(title);
-
-            // Hit - https://en.wikipedia.org/w/api.php?action=query&titles={title}&format=json&prop=images and get a list back of all the images on the page
-            // Take the first image that isn't an svg. otherwise take svgs.  If no images set the image to a placeholder
-
-            // Make another request to the info API and suck off all the Title, description and paragraph text
-
-            // build up the page using the extracted info
-
-            // Use Oktokit to open a PR with the newly created file added to the _posts folder
-
-            return new JsonResult(data);
+            return new JsonResult(pullRequest);
         }
     }
 }
